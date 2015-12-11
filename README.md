9cb8dd0dbe6e231b58b7ee4e4a862086f425c5e2
Trevor Blanchard <studentblanchard@gmail.com>
2015-12-10 22:20:39 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# MovieTracker
+
+### Purpose
+A python module that stores IMDB movie data on entered movies and provides an interface for retrieving metrics on the stored data.
+
+### interface
+```
+> search "movie title"
+...
+> get avgrate
+...
+> get avgyear
+...
+> get topactor
+...
+```
+
+### Sources
+This module uses the [OMdb API](http://www.omdbapi.com/).
---
+++ mtrack.py
@@ -0,0 +1,37 @@
+## - Python movie tracker module
+
+
+import sys
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+url = "http://www.omdbapi.com/?"
+
+def searchmovie(title):
+    ## - returns a parsed dataset retrieved from the API
+    pass
+
+def save(data):
+    ## - store parsed data in the log
+    pass
+
+def getavgrate():
+    ## - Returns the average rating from all the movies in the log
+    pass
+
+def getavgyear():
+    ## - Returns the average rating from all the movies in the log
+    pass
+
+def gettopactor():
+    ## - Returns the most frequent actor in the log
+    pass
+
+def main():
+    ## - Main entry point for the script
+    print("Done.")
+    pass
+
+if __name__ == '__main__':
+    sys.exit(main())
